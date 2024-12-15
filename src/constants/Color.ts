8384c57f7949cf3337a9e7036cebe8aa580fe4a0
thuhuong0108 <huong220108@gmail.com>
2024-12-15 17:16:11 +0700

    color

--- src/constants/Color.ts
+++ src/constants/Color.ts
@@ -1,5 +1,33 @@
-const colors  = {
-    primary: "#1B149B",
-    secondary: "#726EC9",
+const colors = {
+  primary: "#1B149B",
+  secondary: "#726EC9",
+  background: "#FBFBFB",
 
-}
+  status: {
+    pending: "#F9D423",
+    pendingline: "#FFF7CE",
+    process: "#FEF2E5",
+    processline: "#CD6200",
+    cancel: "#FFCFCE",
+    cancelline: "#D32F2F",
+    complete: "#EBF9F1",
+    completeline: "#1F9254",
+  },
+
+  toastcolor: {
+    success: "#07bc0c",
+    warning: "#f1c40f",
+    error: "#e74c3c",
+  },
+
+  buttoncolor: {
+    close: "#aaa",
+    primary: "#0062CC",
+    red: "#CC0000",
+  },
+
+  textprimary: "#000000",
+  textwhite: "#fff",
+  textgray: "#707070",
+};
+export default colors;
