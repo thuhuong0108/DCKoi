80c846d6e8873e9e01bfb51f378e8c228e46c6ee
Hoàng Việt <viethxse@gmail.com>
2025-03-11 09:09:53 +0700

    feat(project) vietnamse form

--- src/validations/validate.ts
+++ src/validations/validate.ts
@@ -11,77 +11,92 @@ export interface ParseResult {
 }
 
 export const validateRegister = yup.object().shape({
-  fullName: yup.string().required(),
-  email: yup.string().email().required(),
-  phone: yup.string().required(),
-  password: yup.string().required(),
-  address: yup.string().required(),
-  dob: yup.date().required(),
-  gender: yup.string().required(),
+  fullName: yup.string().required("Yêu cầu nhập họ tên"),
+  email: yup
+    .string()
+    .email("Email không hợp lệ")
+    .required("Yêu cầu nhập email"),
+  phone: yup.string().required("Yêu cầu nhập số điện thoại"),
+  password: yup.string().required("Yêu cầu nhập mật khẩu"),
+  address: yup.string().required("Yêu cầu nhập địa chỉ"),
+  dob: yup.date().required("Yêu cầu nhập ngày sinh"),
+  gender: yup.string().required("Yêu cầu chọn giới tính"),
 });
 
 export const validateLogin = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
+  email: yup
+    .string()
+    .email("Email không hợp lệ")
+    .required("Yêu cầu nhập email"),
+  password: yup.string().required("Yêu cầu nhập mật khẩu"),
 });
 
 export const validatePackageItem = yup.object().shape({
-  name: yup.string().required(),
+  name: yup.string().required("Yêu cầu nhập thông tin"),
 });
 
 export const validateEquipment = yup.object().shape({
-  name: yup.string().required(),
+  name: yup.string().required("Yêu cầu nhập tên thiết bị"),
   description: yup.string(),
 });
 
 export const validateService = yup.object().shape({
-  name: yup.string().required(),
+  name: yup.string().required("Yêu cầu nhập tên dịch vụ"),
   description: yup.string(),
-  price: yup.number().required(),
-  unit: yup.string().required(),
-  type: yup.string().required(),
+  price: yup.number().required("Yêu cầu nhập giá"),
+  unit: yup.string().required("Yêu cầu nhập đơn vị"),
+  type: yup.string().required("Yêu cầu chọn loại dịch vụ"),
 });
 
 export const validateStaff = yup.object().shape({
-  fullName: yup.string().required(),
-  email: yup.string().email().required(),
-  password: yup.string().required(),
-  phone: yup.string().required(),
-  position: yup.string().required(),
+  fullName: yup.string().required("Yêu cầu nhập họ tên"),
+  email: yup
+    .string()
+    .email("Email không hợp lệ")
+    .required("Yêu cầu nhập email"),
+  password: yup.string().required("Yêu cầu nhập mật khẩu"),
+  phone: yup.string().required("Yêu cầu nhập số điện thoại"),
+  position: yup.string().required("Yêu cầu nhập chức vụ"),
 });
 
 export const validatePackage = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-  price: yup.number().required(),
+  name: yup.string().required("Yêu cầu nhập tên gói"),
+  description: yup.string().required("Yêu cầu nhập mô tả"),
+  price: yup.number().required("Yêu cầu nhập giá"),
 });
 
 export const validateTemplateConstruction = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
+  name: yup.string().required("Yêu cầu nhập tên mẫu"),
+  description: yup.string().required("Yêu cầu nhập mô tả"),
 });
 
 export const validateEstimatePrice = yup.object().shape({
-  area: yup.number().required().min(0),
-  depth: yup.number().required().min(0),
+  area: yup
+    .number()
+    .required("Vui lòng nhập diện tích")
+    .min(0, "Diện tích không được nhỏ hơn 0"),
+  depth: yup
+    .number()
+    .required("Vui lòng nhập độ sâu")
+    .min(0, "Độ sâu không được nhỏ hơn 0"),
 });
 export const validateRequestProject = yup.object().shape({
-  customerName: yup.string().required(),
-  district: yup.string().required(),
-  email: yup.string().email().required(),
-  ward: yup.string().required(),
-  address: yup.string().required(),
-  phone: yup.number().required(),
-  area: yup.number().required(),
-  depth: yup.string().required(),
-  packageId: yup.string().required(),
+  customerName: yup.string().required("Yêu cầu nhập tên khách hàng"),
+  district: yup.string().required("Yêu cầu chọn quận"),
+  email: yup.string().email().required("Yêu cầu nhập email"),
+  ward: yup.string().required("Yêu cầu chọn phường"),
+  address: yup.string().required("Yêu cầu nhập địa chỉ"),
+  phone: yup.number().required("Yêu cầu nhập số điện thoại"),
+  area: yup.number().required("Yêu cầu nhập diện tích"),
+  depth: yup.string().required("Yêu cầu nhập độ sâu"),
+  packageId: yup.string().required("Yêu cầu chọn gói dịch vụ"),
 });
 
 export const validateDeny = yup.object().shape({
-  reason: yup.string().required(),
+  reason: yup.string().required("Yêu cầu nhập lý do"),
 });
 export const validateContract = yup.object().shape({
-  name: yup.string().required(),
-  customerName: yup.string().required(),
-  url: yup.string().required(),
+  name: yup.string().required("Yêu cầu nhập tên hợp đồng"),
+  customerName: yup.string().required("Yêu cầu nhập tên khách hàng"),
+  url: yup.string().required("Yêu cầu hợp đồng"),
 });
